719fe1e664291631ddb8ec706fa00dfe92097247
Micah Roberson <micah.roberson@gmail.com>
2015-01-12 07:44:06 -0800

    Change up image url reference

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,24 +1,8 @@
 $(document).ready(function() {
   var images = [
-    'images/DSC00049.jpg',
-    'images/DSC00053.jpg',
-    'images/DSC00047.jpg'
+    '/images/DSC00049.jpg',
+    '/images/DSC00053.jpg',
+    '/images/DSC00047.jpg'
   ];
-  // var current = 0;
-  // function timeout () {
-  //   setTimeout(function () {
-  //     if (current < images.length - 1) {
-  //       current += 1;
-  //     } else {
-  //       current = 0;
-  //     }
-  //     var url = images[current];
-  //     console.log(current);
-  //     console.log(url);
-  //     document.body.parentNode.setAttribute('style', 'background: url("' + url + '") no-repeat center center fixed;-webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;');
-  //     timeout();
-  //   }, 3000);
-  // }
-  // timeout();
   $.backstretch(images, {duration: 6000, fade: 1500});
 });
