b084e539c9bb8a20bb82e6c0522579a543e8b575
Micah Roberson <micah.roberson@gmail.com>
2015-01-12 07:45:35 -0800

    Rename image endings

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var images = [
-    '/images/DSC00049.jpg',
-    '/images/DSC00053.jpg',
-    '/images/DSC00047.jpg'
+    '/images/DSC00049.JPG',
+    '/images/DSC00053.JPG',
+    '/images/DSC00047.JPG'
   ];
   $.backstretch(images, {duration: 6000, fade: 1500});
 });
